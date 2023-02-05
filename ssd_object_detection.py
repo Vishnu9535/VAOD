c5e9c6ce1127a566b314c0665eb2b018eba24b07
Vishnu9535 <vk9255888@gmail.com>
2023-02-05 13:10:56 +0530

    object-detection

---
+++ ssd_object_detection.py
@@ -0,0 +1,71 @@
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+use_gpu = True
+live_video = False
+confidence_level = 0.5
+fps = FPS().start()
+ret = True
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor",]
+
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+net = cv2.dnn.readNetFromCaffe('ssd_files/MobileNetSSD_deploy.prototxt', 'ssd_files/MobileNetSSD_deploy.caffemodel')
+
+if use_gpu:
+    print("[INFO] setting preferable backend and target to CUDA...")
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+
+print("[INFO] accessing video stream...")
+if live_video:
+    vs = cv2.VideoCapture(0)
+else:
+    vs = cv2.VideoCapture('sample.mp4')
+object_name=[]
+accuracy=[]
+while ret:
+    ret, frame = vs.read()
+    if ret:
+        frame = imutils.resize(frame, width=400)
+        (h, w) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
+        net.setInput(blob)
+        detections = net.forward()
+        
+        for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > confidence_level:
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                # print(CLASSES[idx],confidence * 100)
+                # object_name[CLASSES[idx]]=confidence*100
+                # object_name.append(CLASSES[idx])
+                # accuracy.append(confidence*100)
+                label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
+
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+        
+        frame = imutils.resize(frame,height=400)
+        cv2.imshow('Live detection',frame)
+
+        if cv2.waitKey(1)==27:
+            break
+
+        fps.update()
+
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# print(len(object_name))
